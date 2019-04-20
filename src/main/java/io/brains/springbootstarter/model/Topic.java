fb28f1715b813147c262b308f4d113a3b320897a
punee <punee@DESKTOP-1C8EPLS.frontierlocal.net>
2019-04-19 23:46:49 -0600

    Added swagger configuration

--- src/main/java/io/brains/springbootstarter/model/Topic.java
+++ src/main/java/io/brains/springbootstarter/model/Topic.java
@@ -1,9 +1,13 @@
 package io.brains.springbootstarter.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class Topic {
-	
+	@ApiModelProperty(notes = "The database generated topic ID")
 	private String id;
+	 @ApiModelProperty(notes = "The database generated topic name")
 	private String name;
+	 @ApiModelProperty(notes = "The database generated topic description")
 	private String description;
 	
 	
